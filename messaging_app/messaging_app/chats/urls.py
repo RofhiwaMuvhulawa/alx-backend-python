5ba75d49a181b73ebf42db50bd4cc9b965bd0617
Rofhiwa Muvhulawa <rofhiwamuvhulawa13@gmail.com>
2025-07-19 20:02:42 +0200

    Delete messaging_app/messaging_app directory

--- messaging_app/messaging_app/chats/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('chatrooms/', views.ChatRoomList.as_view(), name='chatroom-list'),
-    path('chatrooms/<int:pk>/', views.ChatRoomDetail.as_view(), name='chatroom-detail'),
-]
