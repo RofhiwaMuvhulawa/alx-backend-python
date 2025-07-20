82cf6c1d4359dcc4523f32ea2bccc97a53672809
Rofhiwa Muvhulawa <rofhiwamuvhulawa13@gmail.com>
2025-07-20 08:15:03 +0200

    Update serializers.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,12 +1,36 @@
 from rest_framework import serializers
-from .models import ChatRoom, Message
+from django.contrib.auth import get_user_model
+from .models import Conversation, Message
+from rest_framework.exceptions import ValidationError
 
-class ChatRoomSerializer(serializers.ModelSerializer):
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ChatRoom
-        fields = ['id', 'name', 'created_at']
+        model = User
+        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number']
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+    message_body = serializers.CharField()
+
     class Meta:
         model = Message
-        fields = ['id', 'chat_room', 'user', 'content', 'timestamp']
+        fields = ['message_id', 'conversation', 'sender', 'message_body', 'sent_at']
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True, source='messages')
+    participant_usernames = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'participant_usernames', 'messages', 'created_at']
+
+    def get_participant_usernames(self, obj):
+        return [user.username for user in obj.participants.all()]
+
+    def validate_participants(self, value):
+        if len(value) < 2:
+            raise ValidationError("A conversation must have at least two participants.")
+        return value
