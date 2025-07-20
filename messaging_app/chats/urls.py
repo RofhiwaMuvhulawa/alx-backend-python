708b66c250881237ba48ef0d06c1830336cd3cf2
Rofhiwa Muvhulawa <rofhiwamuvhulawa13@gmail.com>
2025-07-20 08:19:24 +0200

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'conversations', views.ConversationViewSet, basename='conversation')
+router.register(r'conversations/(?P<conversation_id>[^/.]+)/messages', views.MessageViewSet, basename='message')
+
 urlpatterns = [
-    path('chatrooms/', views.ChatRoomList.as_view(), name='chatroom-list'),
-    path('chatrooms/<int:pk>/', views.ChatRoomDetail.as_view(), name='chatroom-detail'),
+    path('', include(router.urls)),
 ]
