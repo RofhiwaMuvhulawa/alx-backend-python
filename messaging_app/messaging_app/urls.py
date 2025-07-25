270202c45c01a0b76c70375d401d6c2c4a102dde
Rofhiwa Muvhulawa <rofhiwamuvhulawa13@gmail.com>
2025-07-25 13:57:52 +0200

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', include('rest_framework_simplejwt.urls')),  # JWT endpoints
 ]
